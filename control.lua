095a324662d738c055fbe68b34b12184ef409661
Florian Gebhardt <me@fgardt.dev>
2022-12-07 20:54:37 +0100

    fix init

--- control.lua
+++ control.lua
@@ -161,7 +161,10 @@ local init = function()
     ensure_globals()
     pathfinder.init_globals()
 
-    gui.init()
+    for _, player in pairs(game.players) do
+        gui.init(player)
+    end
+
     gui_handler.init(gui)
 end
 
